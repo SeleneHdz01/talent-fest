b0cb7f3f29b594859960114d14d05f100352ec44
SeleneHdz01 <hdz_f@hotmail.com>
2019-08-03 10:42:10 -0500

    Updates

---
+++ precache-manifest.938702f5e70b69c88e83bb1cd090ad84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8737a0c37854f3a66cb2efe53e484094",
+    "url": "/talent-fest/index.html"
+  },
+  {
+    "revision": "f660bde1348e6e86e978",
+    "url": "/talent-fest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/talent-fest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f660bde1348e6e86e978",
+    "url": "/talent-fest/static/js/main.d174a923.chunk.js"
+  },
+  {
+    "revision": "b251e942997e3fb71761",
+    "url": "/talent-fest/static/js/runtime~main.b0969795.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/talent-fest/static/media/logo.5d5d9eef.svg"
+  }
+]);
