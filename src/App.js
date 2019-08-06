57c476df2709a91327fc66049b44b6f1f0ed18af
cristalcabrera95 <cristalcabrera9507@gmail.com>
2019-08-06 11:05:26 -0500

    ya sirven todas las rutas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-import Dashboard from "./Components/Dashboard";
+import SiderDemo from "./Components/Dashboard";
 import "./App.css";
 import Login from "./Components/Login";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      {/* <Dashboard /> */}
-    </div>
+    <React.Fragment>
+       
+      <Route  exact path="/" component={Login}/>
+      <Route  path="/dashboard" component={SiderDemo}/>
+      
+    
+       
+     </React.Fragment>  
   );
 }
 
