e21242047f94fe3ebba72801fa3553e3800f6e6d
SeleneHdz01 <hdz_f@hotmail.com>
2019-08-06 16:23:49 -0500

    Caracter fantasma eliminado

--- src/Components/Clientes.js
+++ src/Components/Clientes.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Chart from "chart.js";
-import randomColor from 'randomcolor'
-​
+import randomColor from "randomcolor";
+
 class Layout extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tabla: []
-    }
+    };
   }
   componentDidMount() {
     const node = this.node;
@@ -17,47 +17,44 @@ class Layout extends React.Component {
       })
       .then(data => {
         console.log("customer", data);
-        let labels =[];
+        let labels = [];
         let count_data = [];
-        let tableData =[];
+        let tableData = [];
         data.map(customer => {
-            if(labels.indexOf(customer.bussinesActivity) ==-1){
-                labels.push(customer.bussinesActivity)
-            }
-            tableData.push(
-              <tr>
-                <td>{customer.companyName}</td>
-                <td>{customer.first_name}</td>
-                <td>{customer.lastLogin}</td>
-              </tr>
-            )
+          if (labels.indexOf(customer.bussinesActivity) == -1) {
+            labels.push(customer.bussinesActivity);
+          }
+          tableData.push(
+            <tr>
+              <td>{customer.companyName}</td>
+              <td>{customer.first_name}</td>
+              <td>{customer.lastLogin}</td>
+            </tr>
+          );
         });
         this.setState({
           tabla: tableData
-        
-        })
-        labels.map(value =>{
-            var count =0;
-            data.map(customer => {
-                if(value==customer.bussinesActivity){
-                    count++
-                }
-                
-            });
-          count_data.push(count)  
-        })
-​
+        });
+        labels.map(value => {
+          var count = 0;
+          data.map(customer => {
+            if (value == customer.bussinesActivity) {
+              count++;
+            }
+          });
+          count_data.push(count);
+        });
         new Chart(node, {
           type: "bar",
           data: {
-            labels:labels,
+            labels: labels,
             datasets: [
               {
                 label: "Sin actividad",
                 data: count_data,
                 backgroundColor: randomColor({
-                    luminosity: 'light', 
-                    count: labels.length
+                  luminosity: "light",
+                  count: labels.length
                 })
               }
             ]
@@ -69,41 +66,37 @@ class Layout extends React.Component {
             }
           }
         });
-        
       });
   }
   render() {
     return (
       <div>
-          <h1>Número de Clientes: 100</h1>
+        <h1>Número de Clientes: 100</h1>
         <canvas
           style={{ width: 800, height: 300 }}
           ref={node => (this.node = node)}
         />
-      <div>
-      <div className="row">
-      <div className="col-md-12">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Compañia</th>
-            <th>Nombre del Cliente</th>
-            <th>Ultima Conexion</th>
-            
-          </tr>
-        </thead>
-        <tbody id="tabla">
-        {
-          this.state.tabla.map (tr => { 
-            return tr
-          })
-        }
-​
-        </tbody>
-      </table>
-      </div>
-      </div>  
-      </div>
+        <div>
+          <div className="row">
+            <div className="col-md-12">
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Compañia</th>
+                    <th>Nombre del Cliente</th>
+                    <th>Ultima Conexion</th>
+                  </tr>
+                </thead>
+                <tbody id="tabla">
+                  {this.state.tabla.map(tr => {
+                    return tr;
+                  })}
+                  ​
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
