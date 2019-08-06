dd4e18193f5361b23aad47120a10892af33bd2ef
cristalcabrera95 <cristalcabrera9507@gmail.com>
2019-08-06 15:08:33 -0500

    grafica de clientes

--- src/Components/Clientes.js
+++ src/Components/Clientes.js
@@ -1,11 +1,76 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Chart from "chart.js";
+import randomColor from 'randomcolor'
 
-export default class Clientes extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Clientes</h1>
-            </div>
-        )
-    }
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    const node = this.node;
+    fetch(`https://guarded-harbor-23202.herokuapp.com/customer`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log("customer", data);
+        let labels =[];
+        let count_data = [];
+        let count_id =[];
+        data.map(customer => {
+            if(labels.indexOf(customer.bussinesActivity) ==-1){
+                labels.push(customer.bussinesActivity)
+            }
+        });
+        labels.map(value =>{
+            var count =0;
+            data.map(customer => {
+                if(value==customer.bussinesActivity){
+                    count++
+                }
+                
+            });
+          count_data.push(count)  
+        })
+
+        new Chart(node, {
+          type: "bar",
+          data: {
+            labels:labels,
+            datasets: [
+              {
+                label: "Sin actividad",
+                data: count_data,
+                backgroundColor: randomColor({
+                    luminosity: 'light', 
+                    count: labels.length
+                })
+              }
+            ]
+          },
+          options: {
+            title: {
+              display: true,
+              text: "Actividad Empresarial"
+            }
+          }
+        });
+        
+      });
+  }
+  render() {
+    return (
+      <div>
+          <h1>Número de Clientes: 100</h1>
+        <canvas
+          style={{ width: 800, height: 350 }}
+          ref={node => (this.node = node)}
+        />
+      <div>
+          
+      </div>
+      </div>
+    );
+  }
 }
+export default Layout;
